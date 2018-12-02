acc9fe4da19743e38a823517bac56e87bf9335c6
Ji Tao <jitao75@gmail.com>
2018-12-02 23:34:58 +0800

    add adam's todo model

---
+++ src/app/model.ts
@@ -0,0 +1,31 @@
+let model = {
+    user: "Adam",
+    items [
+        {action: "Buy Flowers", done: false},
+        {action: "Get Shoes", done: false},
+        {action: "Collect Tickets", done: true},
+        {action: "Call Joe", done: false},
+    ]
+}
+
+export class TodoItem {
+    action:string;
+    done:boolean;
+    constructor(action:string, done:boolean){
+        this.action = action;
+        this.done = done
+    }
+}
+
+export class Model {
+    user: string;
+    items: TodoItem[];
+    constructor(){
+        this.user = "Adam";
+        this.items = [
+            new TodoItem("Buy Flowers",false)
+        ]
+        this.items.concat(model.items)
+    }
+
+}
